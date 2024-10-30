d623d5697efb933f54690820be09e0b19c3eece6
s.billordo <sanbillordo@gmail.com>
2024-10-30 18:33:30 +0100

    Ok.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true, // Permite que Vite maneje las rutas como una SPA
+  },
 });
